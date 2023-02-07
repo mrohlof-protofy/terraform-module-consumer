20cf3011011bea9bfb0cfa4067dbcc88ceafb5e1
Markus Rohlof <markus.rohlof@protofy.com>
2023-02-07 19:02:19 +0100

    some trys

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
-module "my_module" {
- source = "https://github.com/mrohlof-protofy/terraform-module/archive/refs/tags/1.0.0.zip"
+module "by_tag" {
+  source = "git@github.com:mrohlof-protofy/terraform-module?ref=1.0.2"
+  var1   = 3
+  var2   = 3
+}
+
+module "released_version_zip" {
+  source = "https://github.com/mrohlof-protofy/terraform-module/archive/refs/tags/1.0.2.zip"
+}
+
+module "released_version_targz" {
+  source = "https://github.com/mrohlof-protofy/terraform-module/archive/refs/tags/1.0.2.tar.gz"
+}
+
+module "github_no_prot" {
+  source = "github.com/mrohlof-protofy/terraform-module?ref=1.0.2"
+  var1   = 3
+  var2   = 3
 }
