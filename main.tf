68f8c08d102516177a4484556f347c1e91fa16ff
Markus Rohlof <markus.rohlof@protofy.com>
2023-02-07 19:12:36 +0100

    by tag an version

--- main.tf
+++ main.tf
@@ -4,16 +4,8 @@ module "by_tag" {
   var2   = 3
 }
 
-module "released_version_zip" {
-  source = "https://github.com/mrohlof-protofy/terraform-module/archive/refs/tags/1.0.2.zip"
-}
-
-module "released_version_targz" {
-  source = "https://github.com/mrohlof-protofy/terraform-module/archive/refs/tags/1.0.2.tar.gz"
-}
-
-module "github_no_prot" {
-  source = "github.com/mrohlof-protofy/terraform-module?ref=1.0.2"
+module "by_version" {
+  source = "git@github.com:mrohlof-protofy/terraform-module?ref=v1.0.2"
   var1   = 3
   var2   = 3
 }
